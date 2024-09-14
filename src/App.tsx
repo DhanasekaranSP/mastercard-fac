e3444fc7fcebde22c909303e6887c7ee0c75da3c
DhanasekaranSP <dspdhana06@gmail.com>
2024-09-14 22:52:18 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { HeroBannerSection } from './HeroBannerSection/HeroBannerSection'
+
+function App() {
+
+  return (
+    <>
+      <HeroBannerSection />
+    </>
+  )
+}
+
+export default App
---
+++ src/HeroBannerSection/HeroBannerSection.tsx
@@ -0,0 +1,22 @@
+import herobannerimage from "../assets/images/HeroBanner.jpg"
+export const HeroBannerSection = () => {
+    return (
+        <div style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url(${herobannerimage})`,
+            backgroundPosition: "center",
+            width: "100%",
+            height: "45vh",
+            backgroundSize: "cover",
+            display: "flex"
+            , justifyContent: "center"
+            , alignItems: "center"
+        }}>
+            <h1 style={{
+                color: "white",
+                textAlign: "center"
+            }}>
+                Find the Perfect Credit or Debit Card - Compare and Choose Your Best Fit
+            </h1>
+        </div>
+    )
+}
